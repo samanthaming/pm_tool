e4939f1cf32cc46721308b6004585d9cf720175a
Samantha <samantha.ming@gmail.com>
2016-02-02 23:02:07 -0800

    #2: build models

---
+++ app/models/discussion.rb
@@ -0,0 +1,4 @@
+class Discussion < ActiveRecord::Base
+
+  validates :title, presence: true
+end
---
+++ app/models/task.rb
@@ -0,0 +1,4 @@
+class Task < ActiveRecord::Base
+
+  validates :title, presence: true, uniqueness: true
+end
