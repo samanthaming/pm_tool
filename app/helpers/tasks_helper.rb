215b49f60f54cc3e217ff75a2999b216dbe3518e
Samantha <samantha.ming@gmail.com>
2016-02-11 19:42:41 -0800

    Refactored with Derek

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,9 @@
 module TasksHelper
 
+  def task_done_button(project, task)
+    text = task.done? ? "undone" : "done"
+    button_to text, mark_done_task_path({id: task, project_id: project}), method: :patch
+  end
 
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -6,6 +6,12 @@ class Task < ActiveRecord::Base
   validates :body, presence: true
   validates :status, numericality: {only_integer: true}
 
+  # scope :done, -> {where(done: true)}
+  # scope :undone, -> {where(done: false)}
+
+  scope :status, -> (status) { where(done: status)  }
+
+
   private
 
   def set_defaults
